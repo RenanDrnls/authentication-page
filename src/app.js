b3bbdfcc69042c83d7e9d66c0e7b35dc4bbb59e8
RenanDrnls <rdorneles64@gmail.com>
2022-03-03 15:38:21 -0300

    Commenting the routes file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
-const mysql = require("mysql");
+//Declaring consts to packages
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const authRoutes = require("./routes/auth")
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "nodelogin"
-});
+//Setting PORT
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,42 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
 
-app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "/login.html"));
-});
-
-app.post("/auth", (request, response) => {
-    const username = request.body.username;
-    const password = request.body.password;
-
-    if(username && password){
-        connection.query("SELECT * FROM accounts WHERE username = ? AND password = ?", [username, password], (error, results, fields) => {
-            if(error) throw error;
-
-            if(results.length > 0) {
-                request.session.loggedin = true;
-                request.session.username = username;
-
-                response.redirect("/home");
-            } else {
-                response.send("Incorrect Username and/or Password!");
-            }
-            response.end();
-        });
-    } else {
-        response.send("Please enter Username and Password!");
-        response.end();
-    }
-});
-
-app.get("/home", (request, response) => {
-    if(request.session.loggedin) {
-        response.send(`Welcome back, ${request.session.username}!`);
-    } else {
-        response.send("Please login to view this page!");
-    }
-    response.end();
-});
+app.use("/user", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
---
+++ src/routes/auth.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+
+//Call the module to connection to the database
+const connection = require("../database/db");
+
+const router = express.Router();
+
+//Login form route
+router.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "../login.html"));
+});
+
+//Post route receiving the form infos from / route
+router.post("/auth", (request, response) => {
+    //Declaring variables with the infos inserted in the form
+    const username = request.body.username;
+    const password = request.body.password;
+
+    //If username and passwor is not empty, search in the database
+    if(username && password){
+        //Query to search with bind fields
+        connection.query("SELECT * FROM accounts WHERE username = ? AND password = ?",
+        [username, password],
+        (error, results, fields) => {
+            
+            //If error is true(have an error), throw the error
+            if(error) throw error;
+
+            //If have one or more results, set the session.loggedin to true,
+            //set the session.username to the username of the user,
+            //Redirect to the home page
+            if(results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect("/home");
+            } else {
+                //If return 0 results of the database, show pass or user incorrect
+                response.send("Incorrect Username and/or Password!");
+            }
+            //End of the response
+            response.end();
+        });
+    } else {
+        //If one of the field is empty
+        response.send("Please enter Username and Password!");
+        response.end();
+    }
+});
+
+//Home route
+router.get("/home", (request, response) => {
+    //If loggedin is set with true, open the home page
+    if(request.session.loggedin) {
+        response.send(`Welcome back, ${request.session.username}!`);
+    } else {
+        //If loggedin is set to false, send a message to make login
+        response.send("Please login to view this page!");
+    }
+    response.end();
+});
+
+module.exports = router;
